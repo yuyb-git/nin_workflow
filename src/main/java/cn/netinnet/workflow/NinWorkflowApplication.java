a8a2f95584c71657b0f2cea4350235e71b2c4179
yyb881109 <yyb881109@.github.com>
2020-03-10 10:25:34 +0800

    init

---
+++ src/main/java/cn/netinnet/workflow/NinWorkflowApplication.java
@@ -0,0 +1,13 @@
+package cn.netinnet.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NinWorkflowApplication.class, args);
+    }
+
+}
