0f7c2a4f2ddc89e0c950f27bd84a8dcd446dda7d
yyb881109 <yyb881109@.github.com>
2020-03-17 10:21:14 +0800

    init

--- src/main/java/cn/netinnet/workflow/activiti/controller/ActivitiController.java
+++ src/main/java/cn/netinnet/workflow/activiti/controller/ActivitiController.java
@@ -6,19 +6,23 @@ import org.activiti.bpmn.converter.BpmnXMLConverter;
 import org.activiti.bpmn.model.BpmnModel;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.Model;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/model")
@@ -26,11 +30,17 @@ public class ActivitiController {
 	
 	private static final Logger log = LoggerFactory.getLogger(ActivitiController.class);
 	 
-    @Autowired
+    @Resource
     ProcessEngine processEngine;
-    @Autowired
+    @Resource
+    RepositoryService repositoryService;
+    @Resource
+    RuntimeService runtimeService;
+    @Resource
+    TaskService taskService;
+    @Resource
     ObjectMapper objectMapper;
-    
+
     @RequestMapping(value = "add")
     public ModelAndView index(HttpSession session){
         ModelAndView mv = new ModelAndView();
@@ -44,7 +54,6 @@ public class ActivitiController {
     @RequestMapping("/modelList")
     @ResponseBody
     public Object modelList(){
-        RepositoryService repositoryService = processEngine.getRepositoryService();
         return repositoryService.createModelQuery().list();
     }
  
@@ -56,23 +65,18 @@ public class ActivitiController {
     public Object deploy(String modelId) throws Exception {
  
         //获取模型
-        RepositoryService repositoryService = processEngine.getRepositoryService();
         Model modelData = repositoryService.getModel(modelId);
         byte[] bytes = repositoryService.getModelEditorSource(modelData.getId());
- 
         if (bytes == null) {
             return "模型数据为空，请先设计流程并成功保存，再进行发布。";
         }
- 
         JsonNode modelNode = new ObjectMapper().readTree(bytes);
- 
         //BpmnModel model = new BpmnJsonConverter().convertToBpmnModel(modelNode);
         BpmnModel model = new BpmnModel();
         if(model.getProcesses().size()==0){
             return "数据模型不符要求，请至少设计一条主线流程。";
         }
         byte[] bpmnBytes = new BpmnXMLConverter().convertToXML(model);
- 
         //发布流程
         String processName = modelData.getName() + ".bpmn20.xml";
         Deployment deployment = repositoryService.createDeployment()
@@ -91,8 +95,12 @@ public class ActivitiController {
     @RequestMapping("/start")
     @ResponseBody
     public Object startProcess(String keyName) {
-        ProcessInstance process = processEngine.getRuntimeService().startProcessInstanceByKey(keyName);
- 
+        //设置assignee的取值，用户可以在界面上设置流程的执行人
+        Map<String, Object> map = new HashMap<>();
+        map.put("assignee0", "张三");
+        map.put("assignee1", "李四");
+        map.put("assignee2", "王五");
+        ProcessInstance process = runtimeService.startProcessInstanceByKey(keyName, map);
         return process.getId() + " : " + process.getProcessDefinitionId();
     }
  
@@ -102,10 +110,9 @@ public class ActivitiController {
     @RequestMapping("/run")
     @ResponseBody
     public Object run(String processInstanceId) {
-        Task task = processEngine.getTaskService().createTaskQuery().processInstanceId(processInstanceId).singleResult();
- 
+        Task task = taskService.createTaskQuery().processInstanceId(processInstanceId).singleResult();
         log.info("task {} find ", task.getId());
-        processEngine.getTaskService().complete(task.getId());
+        taskService.complete(task.getId());
         return "SUCCESS";
     }
 
--- src/test/java/cn/netinnet/workflow/junit/ActivitiJunitTest.java
+++ src/test/java/cn/netinnet/workflow/junit/ActivitiJunitTest.java
@@ -2,10 +2,18 @@ package cn.netinnet.workflow.junit;
 
 import cn.netinnet.workflow.BaseTest;
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.TaskService;
 import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.repository.ProcessDefinitionQuery;
+import org.activiti.engine.task.Task;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.annotation.Rollback;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @ClassName ActivitiJunitTest
@@ -15,11 +23,17 @@ import javax.annotation.Resource;
  */
 public class ActivitiJunitTest extends BaseTest {
 
+    private static final Logger log = LoggerFactory.getLogger(ActivitiJunitTest.class);
+
     @Resource
     RepositoryService repositoryService;
+    @Resource
+    TaskService taskService;
 
     @Test
+    @Rollback(false)
     public void prepare() {
+
         Deployment deployment = repositoryService.createDeployment()//创建一个部署对象
                 .name("请假流程")
                 .addClasspathResource("processes/leaveProcess.bpmn")
@@ -27,6 +41,63 @@ public class ActivitiJunitTest extends BaseTest {
                 .deploy();
         System.out.println("部署ID："+deployment.getId());
         System.out.println("部署名称："+deployment.getName());
+    }
+
+    @Test
+    public void queryProcessDefinition() {
+        String processDefinitionKey = "holiday";
+        //查询流程定义
+        ProcessDefinitionQuery processDefinitionQuery = repositoryService.createProcessDefinitionQuery();
+        List<ProcessDefinition> list = processDefinitionQuery.processDefinitionKey(processDefinitionKey)
+                .orderByProcessDefinitionVersion().desc().list();
+        for (ProcessDefinition processDefinition : list) {
+            log.info("------------------------");
+            log.info("流  程  部  署id：" + processDefinition.getDeploymentId());
+            log.info("流程定义id：" + processDefinition.getId());
+            log.info("流程定义名称：" + processDefinition.getName());
+            log.info("流程定义key：" + processDefinition.getKey());
+            log.info("流程定义版本：" + processDefinition.getVersion());
+        }
+    }
+
+    /**
+     * 删除已经部署成功的流程定义
+     * 背后影响的表:
+     * act_ge_bytearray
+     * act_re_deployment
+     * act_re_procdef
+     */
+    @Test
+    public void deleteDeployment() {
+        String deploymentId = "a2833cf7-10bb-11ea-9ac9-00155d65d6c0";
+        //删除流程定义，如果该流程定义已有流程实例启动则删除时出错
+        //repositoryService.deleteDeployment(deploymentId);
+        //设置true 级联删除流程定义，即使该流程有流程实例启动也可以删除，设置为false非级别删除方式，如果流程
+        repositoryService.deleteDeployment(deploymentId, true);
+    }
+
+    /**
+     * @Author yuyb
+     * @Description 查询某个人的任务
+     * @Date 10:13 2020/3/17
+     * @param
+     * @return void
+     **/
+    @Test
+    public void findPersonalTaskList() {
+        String processDefinitionKey = "holiday2";
+        String assignee = "赵六";
+        List<Task> taskList = taskService.createTaskQuery()
+                .processDefinitionKey(processDefinitionKey)
+                .includeProcessVariables().taskAssignee(assignee).list();
+
+        for (Task task : taskList) {
+            System.out.println("--------------------------------------");
+            System.out.println("流程实例id:" + task.getProcessInstanceId());
+            System.out.println("任务id:" + task.getId());
+            System.out.println("任务负责人:" + task.getAssignee());
+            System.out.println("任务名称:" + task.getName());
+        }
     }
 
 }
